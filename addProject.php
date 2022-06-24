cbff4ef4ed07af4c30af058e970d81c3f15173f7
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-06-24 10:28:58 +0300

    added projects table to add project page

--- addProject.php
+++ addProject.php
@@ -24,7 +24,7 @@ include('configuration.php'); //using database connection file here
         $db->close(); // Close connection
     
         echo "Project added successfully";
-        header("location:projects.php"); // redirects to home page
+        header("location:addProject.php"); // redirects to home page
 
     exit;
     }
@@ -107,25 +107,56 @@ a{
 <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
 
 <div class="row mb-4">
-    <label for="inputName" class="col-sm-2 col-form-label">Project Name:</label>
+    <label for="inputName" class="col-sm-3 col-form-label">Project Name:</label>
     <div class="col-sm-3">
     <input type="name" id="inputName" placeholder="Project Name" name="name" class="form-control">
     </div>
 </div>
 <div class="row mb-4">
-    <label for="inputInstitution" class="col-sm-2 col-form-label">Name of Institution:</label>
+    <label for="inputInstitution" class="col-sm-3 col-form-label">Name of Institution:</label>
     <div class="col-sm-3">
     <input type="institution" id="inputInstitution" placeholder="Name of Institution" name="institution" class="form-control">
     </div>
 </div>
 <div class="row mb-4">
-    <label for="inputImplementationDate" class="col-sm-2 col-form-label">Implementation Date:</label>
+    <label for="inputImplementationDate" class="col-sm-3 col-form-label">Implementation Date:</label>
     <div class="col-sm-3">
     <input type="date" id="inputImplementationDate" placeholder="Implementation Date" name="implementation_date" class="form-control" >
     </div>
 </div>
     <button id="save_btn" type="save" class="btn btn-dark col-sm-1" name="save">Save</button>
 </form>
+</div>
+
+<div class="container">
+
+<h5>Projects</h5>
+
+<table class="table table-dark table-striped table-hover table-bordered">
+<tr> <th>Project Name</th> <th>Institution</th> <th>Implementation Date</th> <th></th></tr>
+
+<?php
+	$str = '';
+	$sql = "SELECT * FROM project ORDER BY implementation_date";
+	$result = mysqli_query($db,$sql);
+	$count = mysqli_num_rows($result);
+
+	while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
+	{
+	$str .= "<tr>
+	<td>".$row['name']."</td>
+	<td>".$row['institution']."</td>
+	<td>".$row['implementation_date']."</td>
+	<td>
+    <a href='editProject.php?id=".$row['project_id']."'>Edit</a>
+    <a href='addTask.php?id=".$row['project_id']."'>Add tasks</a>
+	<a href='deleteProject.php?id=".$row['project_id']."'>Delete</a>
+	</td></tr>";
+	}
+	echo $str;
+?>
+
+</table>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
