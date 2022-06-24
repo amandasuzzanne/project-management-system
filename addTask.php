85364abea8d8ba899509a3a5ed0691c9c216ae9c
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-06-24 03:15:33 +0300

    table displaying all tasks

--- addTask.php
+++ addTask.php
@@ -39,9 +39,22 @@ include('configuration.php'); //using database connection file here
 <h3>Add a new task</h3>
 
 <style>
-    body{background-color:#767c82;}
-    .container{margin-top:3%;}
-    button:hover {opacity: 0.8;}
+body{
+    background-color:#767c82;
+}
+
+.container{
+    margin-top:3%;
+}
+    
+button, a:hover{
+    opacity: 0.8;
+}
+
+a{
+    display: inline-block;
+    padding: 8px 16px;
+}
 </style>
 </head>
 
@@ -66,7 +79,7 @@ include('configuration.php'); //using database connection file here
     <input type="date" id="inputEndDate" placeholder="Date" name="end_date" class="form-control" >
     </div>
 </div>
-
+    <a href="projects.php" class="btn btn-dark col-sm-1">Back</a>
     <button id="save_btn" type="save" class="btn btn-dark col-sm-1" name="save">Save</button>
 </form>
 </div>
@@ -74,7 +87,34 @@ include('configuration.php'); //using database connection file here
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
+<div class="container">
+
+<h3>Tasks</h3>
+
+<table class="table table-dark table-striped table-hover">
+<tr> <th>Task</th> <th>Start Date</th> <th>End Date</th> <th></th> <th></th> </tr>
+
+<?php
+	$str = '';
+	$sql = "SELECT * FROM task ORDER BY start_date";
+	$result = mysqli_query($db,$sql);
+	$count = mysqli_num_rows($result);
+
+	while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
+	{
+	$str .= "<tr>
+	<td>".$row['name']."</td>
+	<td>".$row['start_date']."</td>
+	<td>".$row['end_date']."</td>
+	<td> <a href='editProject.php?id=".$row['project_id']."'>Edit</a> </td>
+	<td> <a href='deleteProject.php?id=".$row['project_id']."'>Delete</a> </td>
+    </tr>";
+	}
+	echo $str;
+?>
+
+</table>
+</div>
+
 </body>
-</html>
-    
-    
+</html> 
