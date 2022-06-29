81e8ad528c7aa7d5ef932612ddfcb04875d39be2
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-06-30 01:06:20 +0300

    change configuration file

--- configuration.php
+++ configuration.php
@@ -2,7 +2,7 @@
 define ('DB_SERVER', 'localhost');
 define ('DB_USERNAME', 'root');
 define ('DB_PASSWORD', '');
-define ('DB_DATABASE', 'projectmgnt_db');
+define ('DB_DATABASE', 'projectmgmt');
 
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
