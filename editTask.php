803326f8a61782d50d3f8920b41d90352c52cb94
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-06-30 17:12:52 +0300

    change form header name

--- editTask.php
+++ editTask.php
@@ -37,7 +37,7 @@ if(isset($_POST['update'])) {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <div class="container">
-<h3>Update Project</h3>
+<h3>Update Task</h3>
 
 <style>
 body{
