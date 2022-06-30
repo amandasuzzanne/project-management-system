e7d99b083a9a09253edb0c940b8cf322454888cb
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-06-30 23:44:57 +0300

    add assigned option

--- addTask.php
+++ addTask.php
@@ -9,18 +9,17 @@
         $name =  $_POST['name'];
         $start_date = $_POST['start_date'];
         $end_date = $_POST['end_date'];
+        $assigned = $_POST['assigned'];
 
         // prepare sql statement
-        $stmt = $db->prepare("INSERT INTO project_task (project_id, name, start_date, end_date) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param('ssss', $project_id, $name, $start_date, $end_date);
+        $stmt = $db->prepare("INSERT INTO project_task (project_id, name, start_date, end_date, assigned) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param('sssss', $project_id, $name, $start_date, $end_date, $assigned);
         $result = $stmt->execute();
+
         // close connection and redirect to url
         if($result) {
-            $stmt->close();
             $db->close(); 
-            // generate url
-            $url = 'addTask.php?project_id=' . $project_id;
-            header("location:" . $url);
+            header("location:" . 'addTask.php?project_id=' . $project_id);
             echo "Task added successfully";
             exit;
         } else echo "<span style='color:red'>Error while adding task</span>";
@@ -78,9 +77,8 @@ a{
     <input type="date" id="inputEndDate" placeholder="Date" name="end_date" class="form-control" >
     </div>
 </div>
-
 <div class="row mb-4">
-    <label for="inputEndDate" class="col-sm-2 col-form-label">Assigned</label>
+    <label for="assigned" class="col-sm-2 col-form-label">Assigned:</label>
     <div class="col-3">
         <select name="assigned" class="form-control">
             <option value="">-- Select User --</option>
@@ -102,38 +100,43 @@ a{
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
 <div class="container">
-
-<h3>Tasks</h3>
-
-<table class="table table-dark table-striped table-hover">
-<tr> 
-    <th>Task</th> 
-    <th>Start Date</th> 
-    <th>End Date</th> 
-    <th></th> 
-    <th></th> 
-</tr>
-
-<?php
-    $project_id = $_GET['project_id']; //get id through query string
-	$str = '';
-	$sql = "SELECT * FROM project_task WHERE project_id=".$project_id;
-	$result = mysqli_query($db, $sql);
-	$count = mysqli_num_rows($result);
-
-	while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-        $str .= "<tr>
-        <td>".$row['name']."</td>
-        <td>".$row['start_date']."</td>
-        <td>".$row['end_date']."</td>
-        <td> <a href='editTask.php?project_id=".$project_id.'&task_id='.$row['id']. "'>Edit</a> </td>
-        <td> <a href='deleteTask.php?id=".$row['id']."'>Delete</a> </td>
-        </tr>";
-	}
-	echo $str;
-?>
-
-</table>
+    <h3>Tasks</h3>
+    <table class="table table-dark table-striped table-hover">
+        <thead>
+            <tr> 
+                <th>Task</th> 
+                <th>Start Date</th> 
+                <th>End Date</th> 
+                <th>Assigned</th> 
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $projects_query = mysqli_query($db, "SELECT * FROM project_task WHERE project_id=" . $_GET['project_id']) ?>
+            <?php while ($row = mysqli_fetch_array($projects_query, MYSQLI_ASSOC)): ?>
+                <tr>
+                    <td><?php echo $row['name'] ?></td>
+                    <td><?php echo $row['start_date'] ?></td>
+                    <td><?php echo $row['end_date'] ?></td>
+                    <td>
+                        <?php 
+                            $users_query = mysqli_query($db, "SELECT * FROM users");
+                            while ($row1 = mysqli_fetch_array($users_query, MYSQLI_ASSOC)) {
+                                if ($row1['id'] == $row['assigned']) {
+                                    echo $row1['first_name'] . ' ' . $row1['last_name'];
+                                }
+                            }
+                        ?>
+                    </td>
+                    <td>
+                        <a href="<?php echo 'editTask.php?project_id=' . $_GET['project_id'] . '&task_id=' . $row['id']; ?>">Edit</a>
+                    </td>
+                    <td><a href="deleteTask.php?task_id=<?php echo $row['id']; ?>">Delete</a></td>
+                </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
 </div>
 
 </body>
