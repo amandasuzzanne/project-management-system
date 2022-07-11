06ee80f04ab19db3fa5b605d97778099eae36df1
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-07-11 23:59:12 +0300

    Update query logic

--- calendar/events.php
+++ calendar/events.php
@@ -2,9 +2,16 @@
 // Using database connection file here
 include('configuration.php'); 
 
-$query = mysqli_query($db, "SELECT name, implementation_date FROM project"); 
-while($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) $events[] = $row;
+$events = array();
+$query = mysqli_query($db, 'SELECT name, implementation_date FROM project'); 
+while($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+    $events[] = array(
+        'title' => $row['name'], 
+        'start' => $row['implementation_date'],
+        'end' => $row['implementation_date'],
+    );
+}
 
-echo json_encode($events);  
+echo json_encode($events); 
 
 ?>  
