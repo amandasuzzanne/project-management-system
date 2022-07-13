ee97863789443bf7f9c13d3daa3e3361fc21a65d
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-07-13 22:31:56 +0300

    Rename page title

--- addProject.php
+++ addProject.php
@@ -38,7 +38,7 @@ include('configuration.php');
 <!DOCTYPE html>
 <html>
 <head>
-<title>New Project</title>
+<title>Add Project</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <div class="container">
