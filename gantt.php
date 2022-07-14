37b1026362a8f055bb7a6ff6c81348aed8470cbb
Dohd <alloicebrian@gmail.com>
2022-07-14 13:19:24 +0300

    Add conditional checks and comments

--- gantt.php
+++ gantt.php
@@ -5,36 +5,38 @@
     // project tasks
     $query = mysqli_query($db, "SELECT * FROM project_task WHERE project_id=" . $_GET['project_id']);
     while ($row = mysqli_fetch_array($query)) $project_tasks[] = $row;
-    // extract values into an array
-    $project_tasks_values = array_map(function ($v) {
-        $a = array_intersect_key($v, array_flip(['id', 'name', 'start_date', 'end_date', 'assigned']));
-        return array_values($a);
+
+    // modify project tasks to obtain only values
+    $task_values = array_map(function ($v) {
+        // extract specific columns
+        $row = array_intersect_key($v, array_flip(['id', 'name', 'start_date', 'end_date', 'assigned']));
+        return array_values($row);
     }, $project_tasks);
+
+    // if null return default empty array
+    if (!$task_values) $task_values = array();
 ?>
 
 <html>
     <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <div class="container">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <style>
+            body{
+                background-color:#767c82;
+            }
 
-<style>
-body{
-    background-color:#767c82;
-}
-
-.container{
-    margin-top:3%;
-}
-    
-button, a:hover{
-    opacity: 0.8;
-}
-</style>
-
+            .container{
+                margin-top:3%;
+            }
+                
+            button, a:hover{
+                opacity: 0.8;
+            }
+        </style>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">
             // parse php variable
-            let tasksValues = <?php echo json_encode($project_tasks_values) ?>;
+            let taskValues = <?php echo json_encode($task_values) ?>;
 
             // google ganttchart
             google.charts.load('current', {'packages':['gantt']});
@@ -51,16 +53,26 @@ button, a:hover{
                 data.addColumn('number', '% Complete');
                 data.addColumn('string', 'Dependencies');
 
-                data.addRows(tasksValues.map(v => {
+                // loop through fetched task values and modify to match
+                // chart columns expected values
+                taskValues = taskValues.map(v => {
                     let a = [...v];
+                    // resource (same as task name)
                     a[2] = v[1];
-                    a[3] = new Date(v[2]);
+                    // start date
+                    a[3] = new Date(v[2]); 
+                    // end date
                     a[4] = new Date(v[3]);
+                    // duration
                     a[5] = Number(a[4]);
+                    // complete percentage
                     a[6] = 0;
-                    a[7] = '';
+                    // dependencies (required and must be null)
+                    a[7] = null;
                     return a;
-                }));
+                });
+
+                data.addRows(taskValues);
 
                 var options = {
                     height: 400,
@@ -70,7 +82,8 @@ button, a:hover{
                 }
 
                 var chart = new google.visualization.Gantt(document.getElementById('chart'));
-                chart.draw(data, options);
+                // if task values exist draw chart
+                if (taskValues.length) chart.draw(data, options);
             }
         </script>
     </head>
