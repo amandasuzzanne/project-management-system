8d398876cd274baf996bc2c75805a7a58c455914
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-08-08 00:39:00 +0300

    Remove internal CSS and add external CSS

--- home.php
+++ home.php
@@ -8,57 +8,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
 </head>
 
-<style>
-h1{
-    text-align: center; 
-}
-
-body {
-  font-family: "Lato", sans-serif;
-  background-color:#767c82;
-  margin-left: 300px;
-}
-
-.sidenav {
-  height: 100%;
-  width: 300px;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: #111;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
-
-.sidenav a {
-  padding: 6px 6px 6px 32px;
-  text-decoration: none;
-  font-size: 25px;
-  color: #818181;
-  display: block;
-}
-
-.sidenav a:hover {
-  color: #f1f1f1;
-}
-
-.container{
-  margin-top:3%;
-}
-
-.main {
- 
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-</style>
-</head>
 <body>
 
 <div class="sidenav">
