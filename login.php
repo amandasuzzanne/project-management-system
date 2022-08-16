c0d32228041e8ad2faac70cca153b2ec58bc19ca
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-08-16 22:20:45 +0300

    indent code

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <?php
 //connect to the database
 require("configuration.php");
-//session to manage
+//initialize session
 session_start(); 
 
 //login and operation
-   $error="";
-      if($_SERVER["REQUEST_METHOD"] == "POST") 
-      {
+$error="";
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
    // extract form request inputs and sanitize
    $email = mysqli_real_escape_string($db,$_POST['email']);
    $password = mysqli_real_escape_string($db,$_POST['password']);
@@ -36,41 +36,39 @@ session_start();
 
 <!DOCTYPE html>
 <html>
-<head>
-<title>Login</title>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-<div class="container">
-<h3>User Login</h3>
-
-<style>
-   body{background-color:#767c82;}
-   .container{margin-top:3%;}
-   button:hover {opacity: 0.8;}
-   
-</style>
-</head>
+<html>
+   <head>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <style>
+         body {
+            background-color: #767c82;
+         }
+         .container {
+            margin-top: 3%;
+         }
+         button:hover {
+            opacity: 0.8;
+         }
+      </style>
+      <title>User Login</title>
+   </head>
 
 <body>
-
-<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-
-<div class="row mb-4">
-   <label for="inputemail" class="col-sm-1 col-form-label">Email:</label>
-   <div class="col-sm-3">
-   <input type="email" class="form-control"  name="email">
-   </div>
-</div>
-<div class="row mb-4">
-   <label for="inputPassword" class="col-sm-1 col-form-label">Password:</label>
-   <div class="col-sm-3">
-   <input type="password" class="form-control" name="password">
-   </div>
-   
-   <div class=""> 
-      <?= $error?: $error; ?>
-   </div>
+      <div class="container">
+         <h3>User Login</h3>
+         <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+            <div class="row mb-4">
+               <label for="inputemail" class="col-sm-1 col-form-label">Email:</label>
+               <div class="col-sm-3">
+                  <input type="email" class="form-control"  name="email" required>
+               </div>
+            </div>
+            <div class="row mb-4">
+               <label for="inputPassword" class="col-sm-1 col-form-label">Password:</label>
+               <div class="col-sm-3">
+                  <input type="password" class="form-control" name="password" required>
+               </div>
+            </div>
             <div class="row mb-4">
                <label for="emp_rank" class="col-sm-1 col-form-label">Rank:</label>
                <div class="col-sm-3">
@@ -82,17 +80,15 @@ session_start();
                         </option>
                      <?php endforeach; ?>  
                   </select>
-</div>
+               </div>
+               <p class="text-danger"> <?= $error?: $error; ?> </p>
+            </div>
+            <button type="submit" class="btn btn-dark col-sm-1" name="login" style="margin-left:50";>Sign in</button>
+            <div><a href="passwordReset.php">Forgot Password?</a></div>
+         </form>
+      </div>
 
-<button type="submit" class="btn btn-dark col-sm-1" name="login" style="margin-left:50";>Sign in</button>
-
-<div><a href="passwordReset.php">Forgot Password?</a></div>
-
-</form>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
-</body>
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+   </body>
 </html>
