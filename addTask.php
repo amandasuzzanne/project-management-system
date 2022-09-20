e3232bd072286f59641023c7a5f0b5e75c1d25bb
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-09-20 11:04:56 +0300

    Rename option name

--- addTask.php
+++ addTask.php
@@ -130,9 +130,9 @@ a{
                         ?>
                     </td>
                     <td>
-                        <a href="<?php echo 'editTask.php?project_id=' . $_GET['project_id'] . '&task_id=' . $row['id']; ?>">Edit</a>
+                        <a href="<?php echo 'editTask.php?project_id=' . $_GET['project_id'] . '&task_id=' . $row['id']; ?>">Edit Task</a>
                     </td>
-                    <td><a href="deleteTask.php?task_id=<?php echo $row['id']; ?>">Delete</a></td>
+                    <td><a href="deleteTask.php?task_id=<?php echo $row['id']; ?>">Delete Task</a></td>
                 </tr>
             <?php endwhile; ?>
         </tbody>
