007593c7c459c965a7dae8fd0ce052cac8da91c5
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-10-05 00:21:27 +0300

    Update delete task query

--- deleteTask.php
+++ deleteTask.php
@@ -3,7 +3,11 @@
 include('configuration.php'); 
 
 //get id through query string
-$id = $_GET['id']; 
+$id = $_GET['task_id']; 
+
+// construct url
+$project_id = $_GET['project_id'];
+$url = 'addTask.php?project_id=' . $project_id;
 
 // delete query
 $deleted = mysqli_query($db, "DELETE FROM project_task WHERE id=".$id); 
@@ -13,7 +17,7 @@ if($deleted)
   //close connection
   $db->close(); 
   //redirects to projects page
-  header("location:addTasks.php"); 
+  header("location:" . $url); 
   echo "Deletion done successfully";
   exit;
 } 
